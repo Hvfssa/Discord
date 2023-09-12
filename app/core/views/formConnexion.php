6f78f05232072e1076731aec81befde19dbd6216
Eikoseijolo <124674906+Eikoseijolo@users.noreply.github.com>
2023-09-12 15:22:50 +0200

    Mise a jour

--- app/core/views/formConnexion.php
+++ app/core/views/formConnexion.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description"
-        content="Page de connexion Discord. Veuillez vous connecter pour acceder à de nombreuses fonctionnalité.">
+        content="Page de connexion Discord. Veuillez vous connecter pour acceder à de nombreuses fonctionnalités.">
     <title>Connexion Discord</title>
-    <link rel="stylesheet" href="./webfiles/sass/style.css">
-    <script src="webfiles/js/script.js" defer></script>
-    <link rel="icon" href="src/img/favicon.png">
+    <link rel="stylesheet" href="./../../public/sass/style.css">
+    <script src="../../public/js/script.js" defer></script>
+    <link rel="icon" href="../../public/src/img/favicon.png">
 </head>
 
-<body>
+<body id="body_connexion">
 
 </body>
 
