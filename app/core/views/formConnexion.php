faa05128bc161c2c0b5628acf3d362a1f8bc0e50
Eikoseijolo <124674906+Eikoseijolo@users.noreply.github.com>
2023-09-12 14:54:03 +0200

    Ajout Formulaire de connexion dans views (en cours de creation)

---
+++ app/core/views/formConnexion.php
@@ -0,0 +1,18 @@
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description"
+        content="Page de connexion Discord. Veuillez vous connecter pour acceder à de nombreuses fonctionnalité.">
+    <title>Connexion Discord</title>
+    <link rel="stylesheet" href="./webfiles/sass/style.css">
+    <script src="webfiles/js/script.js" defer></script>
+    <link rel="icon" href="src/img/favicon.png">
+</head>
+
+<body>
+
+</body>
+
+</html>
