3ca307f7f0f23d602915c8d2f1dc30da1d335228
thedarkonekiller <76535453+thedarkonekiller@users.noreply.github.com>
2023-09-12 15:03:08 +0200

    add models files, add functions for models

--- app/core/models/ChannelModel.php
+++ app/core/models/ChannelModel.php
@@ -1,1 +1,25 @@
 <?php
+
+function getAllChannels() {
+
+}
+
+function getByIdChannel(int $id) {
+
+}
+
+function getByOneChannel(string $word) {
+    
+}
+
+function addChannel(string $name, string $description, string $picture) {
+
+}
+
+function updateChannel(int $id, string $name, string $description, string $picture) {
+
+}
+
+function deleteChannel(int $id) {
+
+}
--- app/core/models/MessageModel.php
+++ app/core/models/MessageModel.php
@@ -1,1 +1,25 @@
 <?php
+
+function getAllMessages() {
+
+}
+
+function getByIdMessage(int $id) {
+
+}
+
+function getByOneMessage(string $word) {
+
+}
+
+function addMessage(string $content) {
+
+}
+
+function updateMessage(int $id, string $content) {
+
+}
+
+function deleteMessage(int $id) {
+
+}
