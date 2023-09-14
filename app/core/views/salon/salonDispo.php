a084cbdb0eece02c6717892d67cc8ff5186a82f1
Eikoseijolo <124674906+Eikoseijolo@users.noreply.github.com>
2023-09-14 15:57:12 +0200

    Ajout formulaire creation de salon et page choix salon

--- app/core/views/salon/salonDispo.php
+++ app/core/views/salon/salonDispo.php
@@ -1,5 +1,23 @@
 <?php
-$pageContent = "Discutez avec vos amis dans ce salon";
-$pageTitle = "Votre salon";
+$pageContent = "Choisissez un salon pour les rejoindre vos amis";
+$pageTitle = "Les salons";
 require_once("../header.php");
 ?>
+<main>
+    <h1 class="white">Salons disponnibles</h1>
+    <section>
+
+    </section>
+    <aside>
+        <div>
+            <img src="#" alt="image du salon">
+            <span><!-- Nom du salon --></span>
+        </div>
+
+
+
+    </aside>
+
+
+
+</main>
