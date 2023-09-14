d56c0802e47e556971c91bfec1d07c0a4b6cd186
AlexisGot <alexisgot@hotmail.fr>
2023-09-14 14:58:45 +0200

    Ajout d'autre fonction dans message

--- app/core/models/MessageModel.php
+++ app/core/models/MessageModel.php
@@ -1,42 +1,63 @@
 <?php
 
-function getAllMessages(){
+function getAllMessages()
+{
     require_once('dbConnect.php');
-    $req = "SELECT m.content, m.createdAt, m.updatedAt, u.pseudo ,u.pfpic FROM messages AS m INNER JOIN users AS u on m.userId = u.userId";
-    $exec = $pdoConn->query($req)->fetchAll(PDO::FETCH_ASSOC);
-    return $exec;
+
+    if ($pdoConn) {
+        $req = "SELECT m.content, m.createdAt, u.pseudo ,u.pfpic FROM messages AS m INNER JOIN users AS u on m.userId = u.userId";
+        $exec = $pdoConn->query($req);
+
+        if($exec){
+            $results = $exec->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
+
+    return $results;
 }
 
-function getByIdMessage(int $id) {
+function getByIdMessage(int $id)
+{
+    require_once('dbConnect.php');
 
+    if ($pdoConn) {
+        $req = "SELECT * FROM messages WHERE messageId=$id";
+        $exec = $pdoConn->query($req);
+
+        if ($exec) {
+            echo "Votre message à été suprimmé avec succès";
+        }
+    }
 }
 
-function getByOneMessage(string $word) {
+// function getByContentMessage(string $content){
+// }
 
-}
+function addMessage(string $content)
+{
+    require_once('dbConnect.php');
 
-function addMessage(string $content) {
-    require_once('dbConnect');
-
-    if($pdoConn){
+    if ($pdoConn) {
         $req = "INSERT INTO messages (content, createdAt, updatedAt) VALUES ('$content','$','$')";
-        
+        $exec = $pdoConne->query($req);
+
+        if ($exec) {
+        } else {
+            echo "Une erreur est survenue, veuillez réessayer ultérieurement";
+        }
     }
 }
 
-function updateMessage(int $id, string $content) {
+function deleteMessage(int $id)
+{
+    require_once('dbConnect.php');
 
-}
+    if ($pdoConn) {
+        $req = "DELETE FROM messages WHERE messageId=$id";
+        $exec = $pdoConn->query($req);
 
-function deleteMessage(int $id) {
-    require_once('dbConnect');
-
-    $req = "DELETE FROM messages WHERE messageId=$id";
-    $exec = $pdoConn->query($req);
-
-    if($exec){
-        echo "Votre Message à bien était supprimer";
+        if ($exec) {
+            echo "Votre Message à bien était supprimer";
+        }
     }
 }
-
-//pdoConn est temporaire
