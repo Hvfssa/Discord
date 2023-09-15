a98f49b7894fa56a9622f470d588ff4c0f7babcf
thedarkonekiller <76535453+thedarkonekiller@users.noreply.github.com>
2023-09-15 15:19:40 +0200

    add description textarea to form

--- app/core/views/channels/add.php
+++ app/core/views/channels/add.php
@@ -5,11 +5,12 @@ $pageTitle = "Formulaire creation de salon";
 
 <section class="body_form">
     <form class="form" method="POST" action="index.php?controller=channel&action=sendAddChannel">
-        <!-- "#" A remplacer vers chemin du controller -->
         <h1>Creation de votre salon</h1>
         <span class="input-span">
             <label for="nom" class="label">Nom de votre serveur</label>
             <input type="text" name="nom"></span>
+            <label for="desc">Description</label>
+            <textarea name="description" id="desc" cols="30" rows="10"></textarea>
         <span class="input-span">
             <label for="image" class="label">Lien de votre image</label>
             <input type="file" name="image"></span>
