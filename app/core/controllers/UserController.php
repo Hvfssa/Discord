bdaefdd2a846eb769754f77ca2d4b9ac4ee9ff14
Simon Givry <simongivry.sg@gmail.com>
2023-09-15 09:36:17 +0200

    Ajout début model user

--- app/core/controllers/UserController.php
+++ app/core/controllers/UserController.php
@@ -1,5 +1,4 @@
-
-<form action="http://discord.test/?controller=User&action=register" method="post">
+<form action="/?controller=User&action=register" method="post">
     <input type="text" name="pseudo" id="a">
     <input type="password" name="mdp" id="b">
     <input type="password" name="mdpVerify" id="c">
@@ -18,12 +17,14 @@ require("./vendor/wixel/gump/gump.class.php");
 // $_POST['mdp'] = 'Azertyuiop1?';
 // var_dump($_POST);
 
-function showRegisterForm(){
+function showRegisterForm()
+{
     // require("./app/core/views/formRegister.php");
     require("./app/core/views/formRegister.php");
 }
 
-function register(){
+function register()
+{
     require("./app/core/models/UserModel.php");
 
     $regex = "/(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[#?!@$%^&*-])(?=.*?[0-9])/";
@@ -32,18 +33,24 @@ function register(){
 
     $gump->validation_rules([
         'pseudo' => 'required|max_len,50|alpha_numeric_dash',
-        'mdp' => 'required|max_len,255|min_len,8|regex,'.$regex,
+        'mdp' => 'required|max_len,255|min_len,8|regex,' . $regex,
         'mdpVerify' => 'required|max_len,255|equalsfield,mdp'
     ]);
 
     $gump->set_fields_error_messages([
-        'pseudo' => ['required'=> 'Veuillez remplir le champ pseudo.',
-                    'max_lengh' => 'Le pseudo ne peut faire que 50 charactère de long.',
-                    'alpha_numeric_dash' => 'Le pseudo ne peut contenir que des charactères aphanumériques, des dash et des underscores.'],
-        'mdp' => ['required' => 'Veuillez remplir ce champ.' ,
-                'min_len' => 'Le mot de passe doit faire au moins 8 charactères'],
-        'mdpVerify' =>['required' => 'Veuillez remplir ce champ',
-                        'equalsfield,mdp' => 'Les mots de passe doivent etre identiques.']
+        'pseudo' => [
+            'required' => 'Veuillez remplir le champ pseudo.',
+            'max_lengh' => 'Le pseudo ne peut faire que 50 charactère de long.',
+            'alpha_numeric_dash' => 'Le pseudo ne peut contenir que des charactères aphanumériques, des dash et des underscores.'
+        ],
+        'mdp' => [
+            'required' => 'Veuillez remplir ce champ.',
+            'min_len' => 'Le mot de passe doit faire au moins 8 charactères'
+        ],
+        'mdpVerify' => [
+            'required' => 'Veuillez remplir ce champ',
+            'equalsfield,mdp' => 'Les mots de passe doivent etre identiques.'
+        ]
 
     ]);
 
@@ -55,76 +62,82 @@ function register(){
 
     $valid_data = $gump->run($_POST);
 
-    // verify username already exists
-    
+    // var_dump($valid_data["pseudo"]);
+
+    if ($gump->errors()) {
+        var_dump($gump->get_readable_errors());
+    } else {
+        if (isset($_POST)) {
+            $pseudo = $valid_data["pseudo"];
+            $alreadyExists = getByOneUser($valid_data["pseudo"]);
+
+            if ($alreadyExists["COUNT(*)"] === 0) {
+
+
+                $mdp = password_hash($valid_data["mdp"], PASSWORD_ARGON2I);
+
+
+                $result = addUser($pseudo, $mdp);
+                if ($result) {
+                    var_dump('redirect');
+                } else {
+                    // var_dump("erreur");
+                }
+            } else {
+                var_dump("Le nom d'utilisateur est déja utilisé");
+            }
+        }
+    }
+}
+
+function showLoginForm()
+{
+    require("./app/core/views/formLogin.php");
+}
+
+function logIn()
+{
+    require("./app/core/models/UserModel.php");
+    $connectGump = new GUMP();
+
+    $connectGump->validation_rules([
+        'pseudo' => ['required' => 'Veuillez remplir le champ pseudo.'],
+        'mdp' => ['required' => 'Veuillez remplir le champ pseudo.'],
+
+    ]);
+
+    $connectGump->set_fields_error_messages([
+        'pseudo' => ['required' => 'Veuillez remplir le champ pseudo.'],
+        'mdp' => ['required' => 'Veuillez remplir ce champ.'],
+    ]);
+
+    $gump->filter_rules([
+        'pseudo' => 'trim|htmlencode',
+        'mdp' => 'trim|htmlencode',
+    ]);
+
+    $valid_data = $gump->run($_POST);
+
+    $userExists = getByOneUser($pseudo);
+
 
     if ($gump->errors()) {
         var_dump($gump->get_readable_errors());
         // redirect form
-    }
-    else {
-        if (getByOneUser($valid_data["pseudo"]) === 0) {
+    } else {
+        if (getByOneUser($valid_data["pseudo"]) === 1 && getByOneUser($valid_data["pseudo"])['mdp'] === $valid_data["mdp"]) {
             var_dump($valid_data["pseudo"]);
             var_dump(password_hash($valid_data["mdp"], PASSWORD_ARGON2I));
+
             addUser($pseudo, $password, $picture);
 
             // creation session
             // redirection page
-        
-        }else {
+
+        } else {
             var_dump("Le nom d'utilisateur est déja utilisé");
         }
     }
-
-}
-
-function showLoginForm(){
-    require("./app/core/views/formLogin.php");
-}
-
-function logIn(){
-    require("./app/core/models/UserModel.php");
-        $connectGump = new GUMP();
-
-        $connectGump->validation_rules([
-            'pseudo' => ['required'=> 'Veuillez remplir le champ pseudo.'],
-            'mdp' => ['required'=> 'Veuillez remplir le champ pseudo.'],
-            
-        ]);
-
-        $connectGump->set_fields_error_messages([
-            'pseudo' => ['required'=> 'Veuillez remplir le champ pseudo.'],
-            'mdp' => ['required' => 'Veuillez remplir ce champ.'],
-        ]);
-
-        $gump->filter_rules([
-            'pseudo' => 'trim|htmlencode',
-            'mdp' => 'trim|htmlencode',
-        ]);
-
-        $valid_data = $gump->run($_POST);
-
-        $userExists = getByOneUser($pseudo);
-        
-    
-        if ($gump->errors()) {
-            var_dump($gump->get_readable_errors());
-            // redirect form
-        }
-        else {
-            if (getByOneUser($valid_data["pseudo"]) === 1 && getByOneUser($valid_data["pseudo"])['mdp'] === $valid_data["mdp"]) {
-                var_dump($valid_data["pseudo"]);
-                var_dump(password_hash($valid_data["mdp"], PASSWORD_ARGON2I));
-
-                addUser($pseudo, $password, $picture);
-    
-                // creation session
-                // redirection page
-            
-            }else {
-                var_dump("Le nom d'utilisateur est déja utilisé");
-            }
-        }
 
 
     // verification et création de session/ cookie
@@ -133,13 +146,15 @@ function logIn(){
 }
 
 
-function signOut(){
+function signOut()
+{
     // destruction des sessions ou cookies
     // redirection page de connexion
 }
 
 
-function showUpdateForm(){
+function showUpdateForm()
+{
     require("./app/core/models/UserModel.php");
 
     getByIdUser($id);
@@ -149,20 +164,21 @@ function showUpdateForm(){
     // require modele user
 
     // select des données de l'utilisateur
-    
+
     // require view formulaire de mise a jour utilisateur 
 
     // appelle fonction modification
-    
+
 }
 
-function modify(){
+function modify()
+{
     require("./app/core/models/UserModel.php");
 
     // controlle données
-    
+
     // modification BDD
-    
+
     // redirection ou message erreur
-    updateUser($id,$pseudo, $mdp, $pfpic);
+    updateUser($id, $pseudo, $mdp, $pfpic);
 }
