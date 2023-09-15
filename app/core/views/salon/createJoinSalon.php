b2f260545999c3f1975348cf41eff6879f5cc154
Valentin1302 <valentingautier8@gmail.com>
2023-09-15 10:56:24 +0200

    SASS createJoinSalon

--- app/core/views/salon/createJoinSalon.php
+++ app/core/views/salon/createJoinSalon.php
@@ -9,7 +9,7 @@
 </head>
 <header>
         <nav>
-            <p>#général</p>
+        <img src="../../../public/src/img/favicon.png" alt="Logo de discord sur le header">
             <ul>
                 <li><a href="./users/formLogin.php">Se Connecter</a></li>
                 <li><a href="./users/formRegister.php">S'inscrire</a></li>
