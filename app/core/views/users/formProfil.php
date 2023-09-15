cb518b65b617c183f572009d69955ce0590f19f7
Malvina666 <marie.lemire@hotmail.com>
2023-09-15 14:25:13 +0200

    ajout des pages profil et formProfil

---
+++ app/core/views/users/formProfil.php
@@ -0,0 +1,11 @@
+<h1>Formulaire de modification du profil</h1>
+<form action="#" method="POST">
+        <div id="profilPict">
+            <p><label for="profilPict">Choisissez votre photo de profil</label></p>
+            <input type="file" name="profilPict" id="profilPict"> 
+        </div>
+        <div id="pseudo">
+            <input type="text" name="pseudo" placeholder="Changez votre pseudo">
+        </div>
+        <button type="submit">Changer de mot de passe</button>
+</form>
---
+++ app/core/views/users/profil.php
@@ -0,0 +1,11 @@
+        <div id="profilPict">
+            <!-- <img src="AJOUTER UPLOAD" alt="photo de profil">  --> 
+        </div>
+        <div id="pseudo">
+            <p><!-- PSEUDO ACTUEL --></p>
+        </div>
+        <a href="index.php?controller=user&action=#">Modifier mon profil</a>
+        <a href="index.php?controller=users&action=#">Modifier mon mot de passe</a>
+    <div id="salon">
+        <p><!-- SALONS --></p>
+    </div>
