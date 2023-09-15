a349c64d42d91036a7b24010f2c009c2d86401c4
thedarkonekiller <76535453+thedarkonekiller@users.noreply.github.com>
2023-09-15 15:16:52 +0200

    sync modify with main branch

--- app/core/views/channels/add.php
+++ app/core/views/channels/add.php
@@ -4,7 +4,7 @@ $pageTitle = "Formulaire creation de salon";
 ?>
 
 <section class="body_form">
-    <form class="form" method="POST" action="#">
+    <form class="form" method="POST" action="index.php?controller=channel&action=sendAddChannel">
         <!-- "#" A remplacer vers chemin du controller -->
         <h1>Creation de votre salon</h1>
         <span class="input-span">
@@ -14,7 +14,7 @@ $pageTitle = "Formulaire creation de salon";
             <label for="image" class="label">Lien de votre image</label>
             <input type="file" name="image"></span>
         <!-- Voir pour afficher l'image si temps libre-->
-        <input class="submit" type="submit" name="envoi" value="Créer"></span>
+        <input class="submit" type="submit" name="submit" value="Créer"></span>
         <a href="../accueil.php">Retour sur la page d'accueil</a>
     </form>
 </section>
